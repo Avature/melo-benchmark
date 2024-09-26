af3e3f0e4371759108a0e03a829e9f8c7299953c
Federico Retyk <fede.retyk@gmail.com>
2024-09-27 00:48:41 +0200

    Print GPU support for PyTorch

--- scripts/reproduce_paper_experiments.py
+++ scripts/reproduce_paper_experiments.py
@@ -230,6 +230,13 @@ def message_gpu_availability():
         print("No GPUs are available.")
     print("\n\n")
 
+    import torch
+
+    print("PyTorch CUDA available:", torch.cuda.is_available())
+    print("Number of GPUs:", torch.cuda.device_count())
+    if torch.cuda.is_available():
+        print("GPU name:", torch.cuda.get_device_name(0))
+
 
 def main():
     parser = argparse.ArgumentParser(
